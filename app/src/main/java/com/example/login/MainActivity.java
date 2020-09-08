6600b1e85b56b66568d511d5ca83f1d07728ff4e
tammieoh <oht@oxy.edu>
2020-09-08 15:10:33 -0700

    init commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+
+//import com.android.volley.Request;
+//import com.android.volley.RequestQueue;
+//import com.android.volley.Response;
+//import com.android.volley.VolleyError;
+
+//import com.android.volley.toolbox.JsonObjectRequest;
+//import com.android.volley.toolbox.Volley;
+//
+//import org.json.JSONArray;
+//import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    Button login_button;
+    EditText usernameView, passwordView;
+    String username, password;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    int counter = 3;
+
+    private Context mContext;
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext = getApplicationContext();
+        textView = (TextView)findViewById(R.id.textView3);
+        login_button = (Button)findViewById(R.id.register_login);
+        usernameView = (EditText) findViewById(R.id.username);
+//        username.getText().toString();
+        passwordView = (EditText)findViewById((R.id.password));
+//        signup_button = (Button)findViewById(R.id.signup);
+//        tx2 = (TextView)findViewById(R.id.textView2);
+//        tx2.setVisibility(View.GONE);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = usernameView.getText().toString();
+                password = passwordView.getText().toString();
+                HashMap<String, String> map1 = new HashMap<String, String>();
+                map1.put("username", username);
+                map1.put("password", password);
+                // create a Request Queue
+                RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+                // initialize a new JsonObjectRequest instance
+                JsonObjectRequest registerUserRequest = null;
+                registerUserRequest = new JsonObjectRequest(Request.Method.POST, "http://192.168.0.15:8080/login", new JSONObject(map1),
+//                        "{\n" +
+//                        "\t\"username\": \"amyoh\",\n" +
+////                                "\t\"username\": \"amyoh\",\n" +
+//                        "\t\"password\": \"amyoh1125\",\n" +
+//                        "\t\"email\": \"amyoh@gmail.com\"\n" +
+//                        "}"),
+                        new Response.Listener<JSONObject>() {
+                            //                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                            //                    Toast .makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                            //                }
+                            //                else {
+                            //                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
+                            //                    tx2.setVisibility(View.VISIBLE);
+                            //                    counter--;
+                            //                    tx2.setText("Attempts Left: " + Integer.toString(counter));
+                            //
+                            //                    if (counter == 0) {
+                            //                        login_button.setEnabled(false);
+                            //                    }
+                            //                }
+                            //                RequestQueue queue =  Volley.newRequestQueue(MainActivity.this);
+                            //                JsonObjectRequest registerUserRequest = new JsonObjectRequest
+                            //                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                //                                queue = MySingleton.getInstance(mCtx).getRequestQueue();
+                                try {
+                                    System.out.println((response.getString("username")) + " successfully logged in!");
+                                    textView.setText(response.getString("username") + " successfully logged in!");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                    System.out.println("Error");
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Do something when error occurred'
+                        JSONObject error_object = null;
+                        if(error.networkResponse.statusCode == 400) {
+                            System.out.println("breakpoint");
+//                            try {
+//                                String error_message = new String(error.networkResponse.data,"UTF-8");
+//                                error_object = new JSONObject(error_message);
+//                            } catch (UnsupportedEncodingException | JSONException e) {
+//                                e.printStackTrace();
+//                            }
+                            try {
+                                if(error_object.getString("message").equals("username not registered")) {
+                                    System.out.println("username doesn't exist");
+                                    textView.setText("Username not registered!");
+                                }
+                                else {
+                                    textView.setText("Username/password is incorrect. Try Again");
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        System.out.println(error.getMessage());
+                        //                           Toast.makeText(mCtx, e + "error", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                // Access the RequestQueue through your singleton class.
+                requestQueue.add(registerUserRequest);
+            }
+        });
+    }
+
+    public void launchSignUpActivity(View view) {
+//        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/login/SignUp.java
@@ -0,0 +1,117 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class SignUp extends AppCompatActivity {
+    Button signup_button, login_button;
+    EditText usernameView, passwordView, emailView;
+    String username, password, email;
+    private TextView textView, textView2;
+
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        mContext = getApplicationContext();
+        textView = (TextView)findViewById(R.id.textView2);
+        textView2 = (TextView)findViewById(R.id.textView3);
+        signup_button = (Button)findViewById(R.id.signup_button2);
+        login_button = (Button) findViewById(R.id.register_login);
+        usernameView = (EditText) findViewById(R.id.username);
+//        username.getText().toString();
+        passwordView = (EditText)findViewById((R.id.password));
+        emailView = (EditText)findViewById((R.id.email));
+
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                System.out.println("hello");
+//                textView.setText("");
+                // initialize a new RequestQueue instance
+            username = usernameView.getText().toString();
+            password = passwordView.getText().toString();
+            email = emailView.getText().toString();
+            HashMap<String, String> map1 = new HashMap<String, String>();
+            map1.put("username", username);
+            map1.put("password", password);
+            map1.put("email", email);
+
+            RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+            // initialize a new JsonObjectRequest instance
+            JsonObjectRequest registerUserRequest = null;
+            registerUserRequest = new JsonObjectRequest(Request.Method.POST, "http://192.168.0.15:8080/registerUser", new JSONObject(map1),
+//                        "{\n" +
+//                        "\t\"username\": \"amyoh\",\n" +
+////                                "\t\"username\": \"amyoh\",\n" +
+//                        "\t\"password\": \"amyoh1125\",\n" +
+//                        "\t\"email\": \"amyoh@gmail.com\"\n" +
+//                        "}"),
+                    new Response.Listener<JSONObject>() {
+                        //                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                        //                    Toast .makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                        //                }
+                        //                else {
+                        //                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
+                        //                    tx2.setVisibility(View.VISIBLE);
+                        //                    counter--;
+                        //                    tx2.setText("Attempts Left: " + Integer.toString(counter));
+                        //
+                        //                    if (counter == 0) {
+                        //                        login_button.setEnabled(false);
+                        //                    }
+                        //                }
+                        //                RequestQueue queue =  Volley.newRequestQueue(MainActivity.this);
+                        //                JsonObjectRequest registerUserRequest = new JsonObjectRequest
+                        //                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            //                                queue = MySingleton.getInstance(mCtx).getRequestQueue();
+                            try {
+                                System.out.println((response.getString("username")) + " successfully registered!");
+                                textView.setText(response.getString("username") + " successfully registered!");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                System.out.println("error");
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // Do something when error occurred
+                    if(error.networkResponse.statusCode == 400) {
+                        textView2.setText("Email already exists!");
+                    }
+                    System.out.println(error.getMessage());
+                    //                           Toast.makeText(mCtx, e + "error", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            // Access the RequestQueue through your singleton class.
+            requestQueue.add(registerUserRequest);
+            }
+        });
+    }
+}
