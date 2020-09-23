e38a49b5a0ca96baa10130a612003b1a4304f14f
tammieoh <oht@oxy.edu>
2020-09-22 20:22:32 -0700

    PopUp Menu
    Created a button for adding entries, and when button is clicked, pop up menu should appear

--- app/src/main/java/com/example/login/HomePage.java
+++ app/src/main/java/com/example/login/HomePage.java
@@ -3,12 +3,38 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomePage extends AppCompatActivity {
+    private FloatingActionButton addEntryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        addEntryButton = (FloatingActionButton) findViewById(R.id.addEntryButton);
+        addEntryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(HomePage.this, addEntryButton);
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(HomePage.this, "You Clicked: " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                popup.show();
+            }
+        });
     }
 }
